952b952248a5d2c2459a7991abacb69a6c750b44
annasjerling <annasjerling@10.8.231.156>
2020-09-24 16:01:02 +0200

    Created classes/interfaces Computor, Instruction, InstructionPointer and Memory

---
+++ Computer/src/computor/InstructionPointer.java
@@ -0,0 +1,18 @@
+package computor;
+
+public class InstructionPointer {
+
+	private int value;
+	
+	public InstructionPointer() {
+		this.value = 0;
+	}
+	
+	protected int value() {
+		return value;
+	}
+	
+	public void jumpTo(int n) {
+		value = n;
+	}
+}
---
+++ Computer/src/computor/Memory.java
@@ -0,0 +1,19 @@
+package computor;
+import java.util.List;
+
+import words.*;
+
+public class Memory {
+
+//	private List<Word> cells;
+//	
+//	public Memory(int size, WordFactory wf) {
+//		for(int i = 0; i < size; i++) {
+//			cells.add(wf.word("0"));
+//		}
+//	}
+//	
+//	public Word read(Address a) {
+//		return cells.getWord(a);
+//	}
+}
