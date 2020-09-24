191a8cd616ef676125801321645c7365f80b6ebf
roddi <roddi@LAPTOP-35HPEDN3>
2020-09-24 18:03:01 +0200

    Fixade Mul och Add

--- Computer/src/computor/Add.java
+++ Computer/src/computor/Add.java
@@ -1,6 +1,10 @@
 package computor;
 public class Add extends BinaryOp {
+	
+	public Add(Operand op1, Operand op2, Address a) {
+		evaluate(op1.getWord(), op2.getWord(), a.getWord());
+	}
 	protected void evaluate(Word w1, Word w2, Word w3) {
-		
+		w3 = w1.add(w2);
 	}
 }
---
+++ Computer/src/computor/Mul.java
@@ -0,0 +1,10 @@
+
+public class Mul extends BinaryOp {
+	public Mul(Operand op1, Operand op2, Address a) {
+		evaluate(op1.getWord(), op2.getWord(), a.getWord());
+	}
+	protected void evaluate(Word w1, Word w2, Word w3) {
+		w3 = w1.mul(w2);
+	}
+
+}
