c8c2aa5d947d6ef7eeb1d6904587dca47c0e54d3
annasjerling <annasjerling@10.8.231.156>
2020-09-24 13:57:31 +0200

    Ändrar tillbaka Ebbas test

--- Computer/src/words/LongWordFactory.java
+++ Computer/src/words/LongWordFactory.java
@@ -4,11 +4,6 @@ public class LongWordFactory implements WordFactory{
 
 	public Word word(String value) {
 		return new LongWord(value);
-	}
-	
-	public void testMethod() {
-		System.out.println("Hejsan");
-	}
-	
+	}	
 
 }
